68816aa174cd6a019e7ed5920003773a82a23d7b
Bekzat Akhmetov <b.akhmetov@astanait.edu.kz>
2020-10-29 01:39:58 +0600

    juyhtggrgtgtg

--- DataTransfer/DTO/ProductCreateDTO.cs
+++ DataTransfer/DTO/ProductCreateDTO.cs
@@ -12,5 +12,6 @@ namespace ProjectForAITUCanteen.DataTransfer.DTO
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsBad { get; set; }
+        //i am mal
     }
 }
